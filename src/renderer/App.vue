307481ad0396119117a3f69850745aa7d35c3d59
王学波 <wangxuebo@gridsum.com>
2018-01-15 10:34:01 +0800

    init: 初始化工程

---
+++ src/renderer/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app" class="window">
+        <div class="window-content">
+            <div class="pane-group">
+                <div class="pane-mini sidebar app__sidebar" style="-webkit-app-region: drag;">
+
+                </div>
+                <div class="pane">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'electron-vue-demo'
+    }
+</script>
+
+<style>
+    /* CSS */
+</style>
